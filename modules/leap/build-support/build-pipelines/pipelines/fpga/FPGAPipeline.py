d5ef39de1d596359d2130adbbff4e58f237cd976
Elliott Fleming <kermin.fleming@intel.com>
2011-02-24 21:12:21 +0000

    CSN: CSN-leap-50
    Added a compile time generated hook to propagate information about the FPGA target to hasim-fpga-control.

--- modules/leap/build-support/build-pipelines/pipelines/fpga/FPGAPipeline.py
+++ modules/leap/build-support/build-pipelines/pipelines/fpga/FPGAPipeline.py
@@ -4,6 +4,7 @@ import SCons.Script
 from model import  *
 from iface_tool import *
 from bsv_tool import *
+from fpga_program_tool import *
 from software_tool import *
 from synthesis_tool import  *
 from post_synthesis_tool import *
@@ -13,13 +14,13 @@ class Build(ProjectDependency):
   def __init__(self, moduleList):
     Iface(moduleList)
     BSV(moduleList)
+    FPGAProgram(moduleList)
     Software(moduleList)
     MCD(moduleList)
     #moduleList.dump()
     Synthesize(moduleList)
     #moduleList.dump()
-    print "finish configuring Synthesize \n"
     PostSynthesize(moduleList)
-    print "finish configuring PostSynthesize \n"
+
 
 
