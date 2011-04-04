ae5b776d966e76d2bdd7b99f8b18269dcb6acaf0
Michael Adler <michael.adler@intel.com>
2011-04-04 23:15:57 +0000

    CSN: CSN-leap-67
    An attempt to get the build pipeline working with the new m5.  I can't actually
    build anything here, so it isn't carefully tested.

--- modules/leap/build-support/build-pipelines/stages/software/software.py
+++ modules/leap/build-support/build-pipelines/stages/software/software.py
@@ -34,14 +34,20 @@ class Software():
     if moduleList.swExe != []:
         inc_paths = moduleList.swIncDir
         libs = moduleList.swLibs
+        whole_libs = []
         cc_flags = host_defs()
         cc_flags += ' ' + cpp_events_flag
         if (getDebug(moduleList)):
-            cc_flags += ' -DASIM_ENABLE_ASSERTIONS'
+            cc_flags += ' -DASIM_ENABLE_ASSERTIONS -DDEBUG'
         if (getTrace(moduleList)):
             cc_flags += ' -DASIM_ENABLE_TRACE'
         cc_flags += ' -DAPM_NAME=\\"' + moduleList.apmName + '\\"'
     
+        if (DEBUG):
+            copt_flags = '-ggdb3 '
+        else:
+            copt_flags = '-g -O2 '
+
         ##
         ## These will be defined if the m5 simulator is part of the model
         ##
@@ -49,44 +55,52 @@ class Software():
         if (M5_BUILD_DIR != ''):
             # m5 needs Python library
             inc_paths += [ os.path.join(sys.exec_prefix, 'include', 'python' + sys.version[:3]) ]
-            python_zip = os.path.join(awb_resolver(M5_BUILD_DIR), 'm5py.zip')
-    
+
             cc_flags += ' -DTRACING_ON=1'
             if (moduleList.env['DEFS']['SIMULATED_ISA'] != ''):
                 cc_flags += ' -DTHE_ISA=' + moduleList.env['DEFS']['SIMULATED_ISA'] + '_ISA'
-    
+
+            ##
+            ## The whole m5 library must be embedded in the final binary because
+            ## the Python code is included as arrays initialized by static
+            ## constructores.
+            ##
+
             if (getDebug(moduleList)):
-                cc_flags += ' -DDEBUG'
-                # Swap the optimized m5 library for a debugging one
+                # First, remove the m5 library from the main list of libraries
+                m5_lib = ''
                 tmp_libs = []
                 for lib in libs:
                     if (os.path.basename(lib) == 'libm5_opt.a'):
-                        tmp_libs += [ os.path.join(os.path.dirname(lib), 'libm5_debug.a') ]
+                        m5_lib = lib
                     else:
                         tmp_libs += [ lib ]
                 libs = tmp_libs
     
-        if (getDebug(moduleList)):
-            copt_flags = '-ggdb3 '
-        else:
-            copt_flags = '-g -O2 '
-    
+                # Second, store the m5 library in whole_libs
+                if (m5_lib != ''):
+                    if (DEBUG):
+                        # Swap the optimized m5 library for a debugging one
+                        m5_lib = os.path.join(os.path.dirname(m5_lib), 'libm5_debug.a')
+                    whole_libs += [ m5_lib ]
+
+
         # CPPPATH defines both gcc include path and dependence path for
         # SCons.  The '#' forces paths to be relative to the root of the build.
         sw_env = moduleList.env.Clone(CCFLAGS = copt_flags + cc_flags,
-                           LINKFLAGS = copt_flags,
-                           CPPPATH = [ '#/' + moduleList.rootDirInc,
-                                       '#/' + moduleList.rootDirSw,
-                                       '#/iface/build/include',
-                                       '.' ] + inc_paths)
+                                      LINKFLAGS = copt_flags,
+                                      CPPPATH = [ '#/' + moduleList.rootDirInc,
+                                                  '#/' + moduleList.rootDirSw,
+                                                  '#/iface/build/include',
+                                                  '.' ] + inc_paths)
     
         sw_env['DEFS']['CWD_REL'] = sw_env['DEFS']['ROOT_DIR_SW_MODEL']
     
         moduleList.env.Export('sw_env')
         sw_build_dir = sw_env['DEFS']['ROOT_DIR_SW'] + '/obj'
         sw_objects = moduleList.env.SConscript([moduleList.rootDirSw + '/SConscript'],
-                                build_dir = sw_build_dir,
-                                duplicate = 0)
+                                               build_dir = sw_build_dir,
+                                               duplicate = 0)
     
         sw_libpath = [ '.' ]
         sw_link_libs = [ 'pthread', 'dl' ]
@@ -95,22 +109,14 @@ class Software():
     
     
         ##
-        ## m5 needs libz, libmysqlclient and Python library
+        ## m5 needs libraries
         ##
         if (M5_BUILD_DIR != ''):
             sw_link_libs += [ 'z' ]
     
-            if (os.path.exists(os.path.join(sys.exec_prefix, 'lib' , 'libmysqlclient.a'))):
-                sw_link_libs += [ 'mysqlclient' ]
-            elif (os.path.exists(os.path.join(sys.exec_prefix, 'lib', 'mysql' , 'libmysqlclient.a'))):
-                sw_link_libs += [ 'mysqlclient' ]
-    
             # m5 needs python
             sw_libpath += [ os.path.join(sys.exec_prefix, 'lib') ]
             sw_link_libs += [ 'python' + sys.version[:3] ]
-    
-            # m5 links a temporary file then adds Python code
-            sw_link_tgt = [ moduleList.swExe[0] + '.no_python' ]
     
     
         ##
@@ -120,16 +126,12 @@ class Software():
         ##
         sw_exe = sw_env.Program(sw_link_tgt, sw_objects + libs + libs, LIBPATH=sw_libpath, LIBS=sw_link_libs)
     
-        if (M5_BUILD_DIR != ''):
-            ##
-            ## Define a simple SCons builder to concatenate files.  Used to append the
-            ## Python zip archive to the executable.
-            ##
-            concat_builder = Builder(action = Action(['cat $SOURCES > $TARGET',
-                                                      'chmod +x $TARGET']))
-            sw_env.Append(BUILDERS = { 'Concat' : concat_builder })
-    
-            # Append Python code to the binary
-            sw_exe = sw_env.Concat(moduleList.swExe, [ sw_exe, python_zip ])
-                                                                                                                                                              
+        ##
+        ## There ought to be a way to tell SCons to include whole libraries as
+        ## part of the link step.  There currently is not.
+        ##
+        if (whole_libs != []):
+            sw_env.Append(LINKFLAGS=['-Wl,--whole-archive'] + whole_libs + ['-Wl,--no-whole-archive'])
+
+
     moduleList.topDependency = moduleList.topDependency + [sw_exe]     
