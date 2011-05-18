4bf6fda92e394796229ce0e6ac5241bd2afeda72
Elliott Fleming <kermin.fleming@intel.com>
2011-05-18 17:09:22 +0000

    CSN: CSN-leap-86
    Extended the multifpga build tool to set up the platform apm in
    private directory.  This is a stepping stone on the road to multifpga
    compilation.

--- modules/leap/build-support/build-pipelines/stages/multifpga/tool.py
+++ modules/leap/build-support/build-pipelines/stages/multifpga/tool.py
@@ -8,10 +8,20 @@ from model import  *
 from config import *
 from multilex import *
 from multiparse import *
+from subprocess import call
+
+
+def makePlatformName(name, apm):
+  return name +'_'+ apm + '_multifpga.apm'
 
 class ParseMultiFPGA():
 
   def __init__(self, moduleList):
+    APM_FILE = moduleList.env['DEFS']['APM_FILE']
+    APM_NAME = moduleList.env['DEFS']['APM_NAME']
+    applicationName = APM_NAME  + '_mutlifpga.apm'
+    applicationPath =  'config/pm/private/' + applicationName
+  
     # build the compiler
     lex.lex()
     yacc.yacc()
@@ -21,5 +31,20 @@ class ParseMultiFPGA():
       print "Found more than one environment file: " + str(envFile) + ", exiting\n"
     environmentDescription = (open(moduleList.env['DEFS']['ROOT_DIR_HW'] + '/' + envFile[0], 'r')).read()
     #print "opened env file: " + moduleList.env['DEFS']['ROOT_DIR_HW'] + '/' + envFile[0] + " -> " + environmentDescription
-    environmentTree = yacc.parse(environmentDescription)
-    print "environment keys: " + str(environmentTree.getPlatformNames)
+    environment = yacc.parse(environmentDescription)
+    print "environment keys: " + str(environment.getPlatformNames)
+
+    # select the connected_app and make it a submodel
+    call(['asim-shell','create', 'submodel', APM_FILE , 'connected_application', applicationPath])        
+
+    # the stuff below here should likely go in a different file, and there will be many hard-coded paths
+    # copy the environment descriptions to private 
+    #APM_FILE
+    #WORKSPACE_ROOT
+    for platformName in environment.getPlatformNames():
+        platform = environment.getPlatform(platformName)
+        platformName = makePlatformName(platform.name,APM_NAME)
+        platformPath = 'config/pm/private/' + platformName
+        call(['asim-shell','cp', platform.path , platformPath])        
+        call(['asim-shell','replace','module', platformPath ,applicationPath])        
+        
