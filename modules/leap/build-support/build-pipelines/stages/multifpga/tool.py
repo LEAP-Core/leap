9ac95bcb43da1a6482ccf174453fc9a055786436
Elliott Fleming <kermin.fleming@intel.com>
2011-07-28 03:06:21 +0000

    CSN: CSN-leap-129
    Upgraded multifpga tool to latest version of ModuleList

--- modules/leap/build-support/build-pipelines/stages/multifpga/tool.py
+++ modules/leap/build-support/build-pipelines/stages/multifpga/tool.py
@@ -80,7 +80,7 @@ class MultiFPGAGenerateLogfile():
       print "leap-configure --pythonize " +  platform.path
       rawDump = Popen(["leap-configure", "--pythonize", "--silent", platform.path], stdout=PIPE ).communicate()[0]
       # fix the warning crap sometimes
-      platformHierarchies[platformName] = ModuleList(moduleList.env, eval(rawDump), moduleList.arguments)
+      platformHierarchies[platformName] = ModuleList(moduleList.env, eval(rawDump), moduleList.arguments, "")
     
     # check that all same named file are the same.  Then we can blindly copy all files to all directories and life will be good. 
     # once that's done, we still need to tell the child about these extra dicts. 
