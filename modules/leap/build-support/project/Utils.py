7fe18d609861952ccb16def25351cad031e4600e
Kermin Fleming <kermin.fleming@intel.com>
2014-12-08 18:37:10 -0500

    Removed definition of relpath.  Four years to adopt Python 2.7 is long enough.
    Added new function for rebasing relative paths, which checks to see if
    the path is absolute before rebasing.

--- modules/leap/build-support/project/Utils.py
+++ modules/leap/build-support/project/Utils.py
@@ -117,26 +117,16 @@ def checkFilePath(prefix, path):
 
 
 ##
-## relpath --
-##     Available as os.path.relpath starting in Python 2.6.  Can remove once
-##     all sites have upgraded.  This code comes from James Gardner's
-##     BareNecessities Python library.
+## rel_if_not_abspath --
+##     Returns a modified relative path according to the start 
+##     directory, or an unmodified absolute path. 
 ##
 
-import posixpath
-
-def relpath(path, start=posixpath.curdir):
-    """Return a relative version of a path"""
-    if not path:
-        raise ValueError("no path specified")
-    start_list = posixpath.abspath(start).split(posixpath.sep)
-    path_list = posixpath.abspath(path).split(posixpath.sep)
-    # Work out how much of the filepath is shared by start and path.
-    i = len(posixpath.commonprefix([start_list, path_list]))
-    rel_list = [posixpath.pardir] * (len(start_list)-i) + path_list[i:]
-    if not rel_list:
-        return posixpath.curdir
-    return posixpath.join(*rel_list)
+def rel_if_not_abspath(path, start=os.curdir):
+    if(path != os.path.abspath(path)):
+        return os.path.relpath(path, start)
+    else:    
+        return path
 
 ##
 ## dictionary_list_create_append -- Checks dictionary to see if key
